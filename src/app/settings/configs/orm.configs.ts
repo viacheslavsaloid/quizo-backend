629c564e3fbe138857f20383fa0c17689398f7f8
viacheslavsaloid <viacheslavsaloid.work@gmail.com>
2021-04-06 20:13:55 +0300

    feat: prod

--- src/app/settings/configs/orm.configs.ts
+++ src/app/settings/configs/orm.configs.ts
@@ -4,17 +4,20 @@ import { ConfigService, ConfigModule } from '@nestjs/config';
 export const ORM_CONFIGS = {
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
-    type: configService.get<'postgres'>('ORM_TYPE'),
-    host: configService.get('ORM_HOST'),
-    port: configService.get<number>('ORM_PORT'),
-    username: configService.get('ORM_USER'),
-    password: configService.get('ORM_PASSWORD'),
-    database: configService.get<string>('ORM_DATABASE'),
-    entities: [__dirname + '/../../../**/*.entity.{js,ts}'],
-    synchronize: configService.get('MODE') === 'dev',
-    ssl: configService.get('MODE') === 'prod' ? {
-      "rejectUnauthorized": false,
-    } : null,
-  })
+  useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+    console.log(configService.get('ORM_TYPE'))
+    return {
+      type: configService.get<'postgres'>('ORM_TYPE'),
+      host: configService.get('ORM_HOST'),
+      port: configService.get<number>('ORM_PORT'),
+      username: configService.get('ORM_USER'),
+      password: configService.get('ORM_PASSWORD'),
+      database: configService.get<string>('ORM_DATABASE'),
+      entities: [__dirname + '/../../../**/*.entity.{js,ts}'],
+      synchronize: configService.get('MODE') === 'dev',
+      ssl: configService.get('MODE') === 'prod' ? {
+        "rejectUnauthorized": false,
+      } : null,
+    }
+  }
 };
